4c0a31a5dd86898d14664596f13fe45bd471e3d8
maximilianvsg <maximilianvsg@gmail.com>
2015-03-10 01:05:46 +0100

    modificaciones alumno 3

--- GitAux.java
+++ GitAux.java
@@ -19,9 +19,12 @@ public class GitAux {
     
     public void metodoAlumno3(){
         System.out.println("metodo 3");
+        System.out.println("modificado por alumno 3"); // alumno 3
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 3
+        System.out.println("Alumno 3 inserta esra sentencia");
     }
 }
